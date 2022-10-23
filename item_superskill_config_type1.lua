92f7f816d506cd0b090d4f79a2c68405acf97b27
v138468 <v138468@gmail.com>
2022-10-23 17:07:20 +0800

    更新 item_superskill_config_type1

--- item_superskill_config_type1.lua
+++ item_superskill_config_type1.lua
@@ -6,15 +6,32 @@ item_superskill_config_type1.name = "item_superskill_config_type1"
 item_superskill_config_type1.role_type = 1
 function item_superskill_config_type1:check()
     local row_number = self:getDataRowCount()
-    print(self.name)
-    print(row_number)
-
     for i = 1, row_number, 1 do
         local row = self:getRowDataByRowNumber(i)
-    --     print(self.name)
-    --     if not text_name:isHasId(row.name_id) then
-    --         print(self.name, row.id)
-    --     end
+        if not text_name:isHasId(row.name_id) then
+            print(self.name, row.id, "name_id")
+        end
+        if not text_content:isHasId(row.desc_id) then
+            print(self.name, row.id, "desc_id")
+        end
+        if row.occupation ~= tostring(self.role_type) then
+            print(self.name, row.id, "occupation")
+        end
+        if not text_ui:isHasId(row.desc_features) then
+            print(self.name, row.id, "desc_features")
+        end
+        if not res_spine:isHasId(SplitString(row.spine_id, "%|")[1]) then
+            print(self.name, row.id, "spine_id")
+        end
+        local star_configs = SplitString(row.star_config, "%|")
+        if #star_configs ~= 10 then
+            print(self.name, row.id, "star_config should have 10 items")
+        end
+        for _, star_config in pairs(star_configs) do
+            if not other_superskill_star:isHasId(star_config) then
+                print(self.name, row.id, "has ", star_config, " miss in other_superskill_star")
+            end
+        end
     end
 end
 
