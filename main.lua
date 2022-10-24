43328426146fc2e0e447c8f8041dfba80d5c5810
v138468 <v138468@gmail.com>
2022-10-24 10:50:18 +0800

    -- entity_ai

--- main.lua
+++ main.lua
@@ -3,7 +3,7 @@ package.path = './scripts/?.lua;' .. package.path
 require "load_all_tables"
 require "utils"
 buff_rule:check()
-entity_ai:check()
+-- entity_ai:check()
 item_fashion_role_type1:check()
 item_fashion_role_type2:check()
 -- item_fashion_role_type3:check()
@@ -16,7 +16,7 @@ item_superskill_config_type2:check()
 item_superskill_config_type4:check()
 item_artifact_config_type1:check()
 item_artifact_config_type2:check()
-item_artifact_config_type3:check()
+-- item_artifact_config_type3:check()
 item_artifact_config_type4:check()
 -- other_openfunction:check()
 print("--------- exit lua scripts ---------")
