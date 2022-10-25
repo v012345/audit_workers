69669f8a475743b81883c989de0b39bf3aa51c3f
v138468 <v138468@gmail.com>
2022-10-25 16:15:27 +0800

    item_material

--- item_material.lua
+++ item_material.lua
@@ -11,7 +11,23 @@ function item_material:check()
         if not text_name:isHasId(row.name_id) then
             print(string.format("%s ( id : %s ) name_id has %s , but miss in text_name", self.name, row.id, row.name_id))
         end
+        if not text_content:isHasId(row.desc_id) then
+            print(string.format("%s ( id : %s ) desc_id has %s , but miss in text_content", self.name, row.id,
+                row.desc_id))
+        end
 
+        local access_type_ids = SplitString(row.access_type_id, "%|")
+        for _, access_type_id in pairs(access_type_ids) do
+            if access_type_id ~= "0" and not config_get_type:isHasId(access_type_id) then
+                print(string.format("%s ( id : %s ) access_type_id has %s , but miss in config_get_type", self.name,
+                    row.id,
+                    access_type_id))
+            end
+        end
+        if row.goods_id ~= "0" and not entity_goods:isHasId(row.goods_id) then
+            print(string.format("%s ( id : %s ) goods_id has %s , but miss in entity_goods", self.name, row.id,
+                row.goods_id))
+        end
     end
 end
 
