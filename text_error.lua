0c5547d771ba0672624c2309104ae70b205aa1e5
v138468 <v138468@gmail.com>
2022-11-07 10:31:33 +0800

    %s 与 %d check

--- text_error.lua
+++ text_error.lua
@@ -10,6 +10,12 @@ function text_error:check()
         if CountIfInString(row.vi, "&") > 0 then
             print(string.format("%s , id = %s , 存在 &", self.name, row.id))
         end
+        if CountIfInString(row.zhcn, "%%d") ~= CountIfInString(row.vi, "%%d") then
+            print(string.format("%s , id = %s , %%d 的数量对匹配", self.name, row.id))
+        end
+        if CountIfInString(row.zhcn, "%%s") ~= CountIfInString(row.vi, "%%s") then
+            print(string.format("%s , id = %s , %%s 的数量对匹配", self.name, row.id))
+        end
     end
 end
 
